f39d09a64595edf01642102fdea7e451831afacb
Bulldogbower <seanbower96@gmail.com>
2022-03-04 15:59:58 -0800

    update

--- LeetCodeExamples/290.WordPattern.py
+++ LeetCodeExamples/290.WordPattern.py
@@ -6,13 +6,10 @@ s=s.split(" ")
 pattern=list(pattern)
 
 #print(pattern,s)
-
+temp=[]
 locations=[]
+match_count=0
+ps=set(pattern)
 
-for location,value in enumerate(pattern):
-    print(location,value)
-    for i in pattern:
-        if i in pattern[location+1:]:
-            temp=pattern.index(i)+1
-            #print(temp)
-            print(pattern[temp:].index(i))
+
+
