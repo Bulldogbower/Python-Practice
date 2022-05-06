d50f2b07667757a5145fc51214153c2e0057a90a
Bulldogbower <seanbower96@gmail.com>
2022-05-06 14:21:20 -0700

    Update

--- LeetCodeExamples/1073.AddingTwoNegabinaryNumbers.py
+++ LeetCodeExamples/1073.AddingTwoNegabinaryNumbers.py
@@ -2,10 +2,10 @@ arr1=[1,1,1,1,1]
 arr2=[1,0,1]
 
 arr1=[0]
-arr2=[1,0]
+arr2=[1,0] #[1,0]
 
 arr1=[0]
-arr2=[1,0]
+arr2=[1,1] #[1,1]
 
 arr1_value=0
 arr2_value=0
@@ -21,23 +21,14 @@ for location, value in enumerate(arr2[::-1]):
         #print("-2^",location, " = ", ((-2)**int(location)))
         arr2_value=arr2_value+((-2)**int(location))
 total=arr1_value+arr2_value
-print(total)
-print(bin(total))
-binary_total=[]
-if total==0:
-    print(0)
-if total>0:
-    binary_total=bin(total)[2:]
 temp=0
-binary_temp=[]
-if total<0:
-    for i in range(int(total))[::-1]:
-        temp=temp+(-2)**i
-        print(temp,total)
-        binary_temp.append("1")
-        if temp==total:
-            break
-print(binary_temp)
-print(binary_total)
+print(total)
+bin_lst=[]
+print(total)
+for location, value in enumerate(range(len(max(arr1,arr2)))[::-1]):
+    print(location,value)  
 
+print(bin_lst)
+bin_lst=bin_lst[::-1]
+#print(bin_lst[bin_lst.index(1):])
 #return(int(arr1_value+arr2_value))
